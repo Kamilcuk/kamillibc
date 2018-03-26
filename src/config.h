1b388f8fa5c615776fe7d625466f5151a0b36dff
Kamil <kamilcukrowski@gmail.com>
2018-03-26 09:44:34 +0200

    Add libraries from net
    Signed-off-by: Kamil <kamilcukrowski@gmail.com>

---
+++ src/config.h
@@ -0,0 +1,15 @@
+/*
+ * config.h
+ *
+ *  Created on: 25 mar 2018
+ *      Author: kamil
+ */
+
+#ifndef SRC_CLIBS_CONFIG_H_
+#define SRC_CLIBS_CONFIG_H_
+
+#if __GNUC__
+#define HAVE_TYPEOF 1
+#endif
+
+#endif /* SRC_CLIBS_CONFIG_H_ */
