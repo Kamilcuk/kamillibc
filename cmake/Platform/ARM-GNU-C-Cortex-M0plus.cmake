d354de70617a901692fccfde3c6bd9b8ea8c032a
Kamil Cukrowski <kamilcukrowski@gmail.com>
2018-05-24 12:16:28 +0200

    Update cmake platform ARM
    Signed-off-by: Kamil Cukrowski <kamilcukrowski@gmail.com>

--- cmake/Platform/ARM-GNU-C-Cortex-M0plus.cmake
+++ cmake/Platform/ARM-GNU-C-Cortex-M0plus.cmake
@@ -1,2 +1,2 @@
 message(STATUS "Configuring for ARM platform, GNU-C compiler, Cortex-M3 processor")
-string(APPEND PROCESSOR_FLAGS " -mcpu=cortex-m0plus -mthumb")
+string(APPEND ARM_PROCESSOR_FLAGS " -mcpu=cortex-m0plus -mthumb")
--- cmake/Platform/ARM-GNU-C.cmake
+++ cmake/Platform/ARM-GNU-C.cmake
@@ -1,15 +1,13 @@
 message(STATUS "Configuring for ARM platform, GNU-C compiler")
 
-set(CMAKE_EXECUTABLE_SUFFIX ".elf")
-
-if (NOT PROCESSOR_FLAGS)
-	MESSAGE(FATAL_ERROR "PROCESSOR_FLASG is not set")
+if(NOT ARM_PROCESSOR_FLAGS)
+	message(FATAL_ERROR "ARM_PROCESSOR_FLAGS is not set")
 endif()
 
-string(APPEND COMPILER_FLAGS " ${PROCESSOR_FLAGS} -ffunction-sections -fdata-sections -fomit-frame-pointer")
-string(APPEND CMAKE_ASM_FLAGS_INIT           " ${COMPILER_FLAGS} -x assembler-with-cpp ")
-string(APPEND CMAKE_C_FLAGS_INIT             " ${COMPILER_FLAGS} ")
-string(APPEND CMAKE_CXX_FLAGS_INIT           " ${COMPILER_FLAGS} ")
+string(APPEND ARM_FLAGS " ${ARM_PROCESSOR_FLAGS} -ffunction-sections -fdata-sections")
+string(APPEND CMAKE_ASM_FLAGS_INIT           " ${ARM_FLAGS} ")
+string(APPEND CMAKE_C_FLAGS_INIT             " ${ARM_FLAGS} ")
+string(APPEND CMAKE_CXX_FLAGS_INIT           " ${ARM_FLAGS} ")
 string(APPEND CMAKE_EXE_LINKER_FLAGS_INIT    " -Wl,--gc-sections ")
 string(APPEND CMAKE_SHARED_LINKER_FLAGS_INIT " -Wl,--gc-sections ")
 string(APPEND CMAKE_MODULE_LINKER_FLAGS_INIT " -Wl,--gc-sections ")
