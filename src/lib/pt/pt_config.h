0a15ac0648a0dea5d1b442aa5cd5596e05ce21c9
Kamil Cukrowski <kamilcukrowski@gmail.com>
2018-06-24 22:50:21 +0200

    Copy original protothreads

---
+++ src/lib/pt/pt_config.h
@@ -0,0 +1,13 @@
+/*
+ * pt_config.h
+ *
+ *  Created on: 24 cze 2018
+ *      Author: kamil
+ */
+
+#ifndef SRC_LIB_PT_PT_CONFIG_H_
+#define SRC_LIB_PT_PT_CONFIG_H_
+
+#define LC_INCLUDE  "lc-addrlabels.h"
+
+#endif /* SRC_LIB_PT_PT_CONFIG_H_ */
